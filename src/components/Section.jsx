df57ffdd0ad1422256e624b4233738e790cd64ed
Lucasmarcelo85 <Devfullmarcelo@gmail.com>
2024-11-20 16:15:37 -0300

    move

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -93,21 +93,22 @@ const Section = () => {
 
       {latestVideoId && (
         <Box mt={3} style={{ width: "100%", paddingTop: "56.25%", position: "relative" }}>
-          <iframe
-            src={`https://www.youtube.com/embed/${latestVideoId}`}
-            title="YouTube video player"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            style={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              borderRadius: "12px",
-            }}
-          ></iframe>
-        </Box>
+        <iframe
+          src={`https://www.youtube.com/embed/${latestVideoId}`}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen"
+          allowFullScreen
+          style={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            borderRadius: "12px",
+          }}
+        ></iframe>
+      </Box>
+      
       )}
     </Box>
   );
